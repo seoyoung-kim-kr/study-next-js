d08822e42dc9059b74d561693ddf820abd9b1ead
seoyoung-kim-kr <seoyoung-kim-kr@gmail.com>
2026-01-18 22:04:17 +0900

    chap4.9: 페이지 미리 생성

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -17,3 +17,12 @@ const PantsPage = async ({ params }: Props) => {
 };
 
 export default PantsPage;
+
+// 함수명은 nextjs에서 정해준 규격 사항임
+// 미리 페이지 만듦 (빌드시)
+export const generateStaticParams = () => {
+  const products = ["pants", "skirt"];
+  return products.map((product) => ({
+    slug: product,
+  }));
+};
