f9a2dae50b401083507aae05a1b19e37e52231ba
seoyoung-kim-kr <seoyoung-kim-kr@gmail.com>
2026-01-18 21:47:42 +0900

    chap4.8: 동적 라우팅

---
+++ src/app/products/[slug]/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+// slug : 내가 설정한 동적 라우터 이름
+// 13+ 버전에서는 App Router의 동적 라우트 params가 비동기 처리되어야 함
+// - params가 Promise로 반환됨
+// async, await로 비동기 처리
+type Props = {
+  params: Promise<{
+    slug: string;
+  }>;
+};
+
+const PantsPage = async ({ params }: Props) => {
+  const { slug } = await params;
+  console.log(slug);
+  return <div>{slug}제품설명페이지</div>;
+};
+
+export default PantsPage;
