599eb1951eaf451472a4564d3fd2003bf5ab0b96
seoyoung-kim-kr <seoyoung-kim-kr@gmail.com>
2026-01-24 17:41:19 +0900

    chap6.9: redirect

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,21 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  // Page가 삭제되거나 이동한 경우, 다른 페이지로 이동켜줘야 하는 경우 페이지 이동하는 것: Redirect
+  async redirects() {
+    return [
+      {
+        source: "/products/deleted_forever", //삭제된 페이지 접속할 경우
+        destination: "/products", //여기로 이동
+        permanent: true, //삭제된 페이지는 영원히 옮겨갔단다 (browser => 308전달 => 이동한 페이지를 캐싱)
+      },
+      {
+        source: "/products/deleted_forever_temp", //삭제된 페이지 접속할 경우
+        destination: "/products", //여기로 이동
+        permanent: false, //일시적으로 페이지 이동한 경우
+      },
+    ];
+  },
 };
 
 export default nextConfig;
