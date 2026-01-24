c6d53447c8174128cac80632e0eb4c2116ca4cf6
seoyoung-kim-kr <seoyoung-kim-kr@gmail.com>
2026-01-24 17:35:04 +0900

    chap6.8: font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { Open_Sans } from "next/font/google";
+import { Nanum_Gothic } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const sans = Open_Sans({ subsets: ["latin"] });
+const gothic = Nanum_Gothic({ weight: "700", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +18,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={sans.className}>
       <body>
-        <header>
+        <header className={gothic.className}>
           Demo note
           <nav>
             <Link href="/products">Products</Link>
