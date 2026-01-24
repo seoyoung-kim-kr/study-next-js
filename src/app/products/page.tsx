582a447e0f669d5da5177cc91b0e608003fef699
seoyoung-kim-kr <seoyoung-kim-kr@gmail.com>
2026-01-24 15:46:07 +0900

    chap5.11: fetch를 사용한 CSR

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 import Link from "next/link";
 import { getProducts } from "@/service/products";
+import MeowArticle from "@/components/MeowArticle";
 
 // export const revalidate = 3;
 
 const ProductsPage = async () => {
   // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
   const products = await getProducts();
-  const res = await fetch("https://meowfacts.herokuapp.com", {
-    // 3초 뒤에 데이터 또 가져와 줘 : 자동 ISR
-    // 0으로 하면 요청할 때 마다 렌더링 됨 (SSR)
-    // cacte : "no-store"도 동일
-    next: { revalidate: 3 },
-  });
-  const data = await res.json();
-  const factText = data.data[0];
 
   return (
     <div>
@@ -26,7 +19,7 @@ const ProductsPage = async () => {
           </li>
         ))}
       </ul>
-      <article className="text-sm bg-green-700 text-white">{factText}</article>
+      <MeowArticle />
     </div>
   );
 };
