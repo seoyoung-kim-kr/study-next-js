5a49a25c579a822c6452878f23e81b29bf10e3c1
seoyoung-kim-kr <seoyoung-kim-kr@gmail.com>
2026-01-24 15:32:30 +0900

    chap5.10: fetc를 사용한 SSG, ISR, SSR

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,11 +2,19 @@ import React from "react";
 import Link from "next/link";
 import { getProducts } from "@/service/products";
 
-export const revalidate = 3;
+// export const revalidate = 3;
 
 const ProductsPage = async () => {
   // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
   const products = await getProducts();
+  const res = await fetch("https://meowfacts.herokuapp.com", {
+    // 3초 뒤에 데이터 또 가져와 줘 : 자동 ISR
+    // 0으로 하면 요청할 때 마다 렌더링 됨 (SSR)
+    next: { revalidate: 3 },
+    // cacthe :
+  });
+  const data = await res.json();
+  const factText = data.data[0];
 
   return (
     <div>
@@ -18,6 +26,7 @@ const ProductsPage = async () => {
           </li>
         ))}
       </ul>
+      <article className="text-sm bg-green-700 text-white">{factText}</article>
     </div>
   );
 };
